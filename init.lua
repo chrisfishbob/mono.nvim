c5e0b2af2bbe5690d7d85dacdc4c5b7aa70555bf
Chris Hsu <chrisfishbob@gmail.com>
2024-04-21 15:41:25 -0700

    Added Copilot and CopilotChat plugins, needs more configuration

--- init.lua
+++ init.lua
@@ -1081,6 +1081,24 @@ require('lazy').setup({
       require('dap-python').setup(debugpy_path)
     end,
   },
+  {
+    'github/copilot.vim',
+  },
+  {
+    {
+      'CopilotC-Nvim/CopilotChat.nvim',
+      branch = 'canary',
+      dependencies = {
+        { 'github/copilot.vim' }, -- or github/copilot.vim
+        { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+      },
+      opts = {
+        debug = true, -- Enable debugging
+        -- See Configuration section for rest
+      },
+      -- See Commands section for default commands if you want to lazy load on them
+    },
+  },
   -- Custom plugins end
 }, {
   ui = {
