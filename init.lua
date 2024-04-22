293708c5918a9c537f2870b2b36bcd2abc161804
Chris Hsu <chrisfishbob@gmail.com>
2024-04-21 18:42:17 -0700

    Disable text wrapping

--- init.lua
+++ init.lua
@@ -72,6 +72,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Disable wrapping
+vim.opt.wrap = false
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
