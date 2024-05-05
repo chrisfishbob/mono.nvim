6c9fc68af3b3310313b2845db46f7c02eb7711f7
Chris Hsu <chrisfishbob@gmail.com>
2024-05-05 10:26:33 -0700

    Commented out AI plugins

--- init.lua
+++ init.lua
@@ -1111,24 +1111,24 @@ require('lazy').setup({
       }
     end,
   },
-  {
-    'github/copilot.vim',
-  },
-  {
-    {
-      'CopilotC-Nvim/CopilotChat.nvim',
-      branch = 'canary',
-      dependencies = {
-        { 'github/copilot.vim' }, -- or github/copilot.vim
-        { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
-      },
-      opts = {
-        debug = true, -- Enable debugging
-        -- See Configuration section for rest
-      },
-      -- See Commands section for default commands if you want to lazy load on them
-    },
-  },
+  -- {
+  --   'github/copilot.vim',
+  -- },
+  -- {
+  --   {
+  --     'CopilotC-Nvim/CopilotChat.nvim',
+  --     branch = 'canary',
+  --     dependencies = {
+  --       { 'github/copilot.vim' }, -- or github/copilot.vim
+  --       { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+  --     },
+  --     opts = {
+  --       debug = true, -- Enable debugging
+  --       -- See Configuration section for rest
+  --     },
+  --     -- See Commands section for default commands if you want to lazy load on them
+  --   },
+  -- },
   -- Custom plugins end
 }, {
   ui = {
